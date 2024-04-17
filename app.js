c59c6eb6c041da1b6e65c2df51979344a748c6c5
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-04-17 21:01:48 +0300

    Final features

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+//Access ball and its container
 const ballArea = document.getElementById('ball-area');
 const ball = document.getElementById('ball');
 
+//Add click event to move the ball
 ballArea.addEventListener('click', (event)=>{
+    //Get coordinates of the click point of the container
     let x = event.clientX;
     let y = event.clientY;
-    ball.style.transform = `translate(${x}px,${y}px)`
+
+    //Add animation style
+    const styling = [
+        { transform:`translate(${x-35}px,${y-35}px)`}
+    ];
+    
+    //Add animation duration
+    const timing = {
+        duration:500
+    }
+    
+    //Apply animation to the ball to move to clicked point
+    ball.animate(styling, timing).onfinish=()=>{
+        ball.style.transform = `translate(${x-35}px,${y-35}px)`;
+    };
 })
